2850f0c558fd59ce36bd40a5093743d2d3011768
Wellton Manopelli <wellton.manopelli@gmail.com>
2023-03-08 15:37:48 -0300

    Criação de exercicios

--- src/br/com/java/exercicios/arrays/Ex_Consoantes.java
+++ src/br/com/java/exercicios/arrays/Ex_Consoantes.java
@@ -7,23 +7,27 @@ public class Ex_Consoantes {
        String[] consoantes = new String[6];
        String letra;
        int cont=0;
+       int qtdConsoantes=0;
        do {
-           letra = JOptionPane.showInputDialog("Insira um numero :");
+           letra = JOptionPane.showInputDialog("Insira uma letra :");
 
 
-           if ((letra.equalsIgnoreCase("a") |
+           if (!(letra.equalsIgnoreCase("a") |
                    letra.equalsIgnoreCase("e") |
                    letra.equalsIgnoreCase("i") |
                    letra.equalsIgnoreCase("o") |
-                   letra.equalsIgnoreCase("u"))) {
+                   letra.equalsIgnoreCase("u"))){
                consoantes[cont]=letra;
+               qtdConsoantes++;
            }
            cont++;
 
        }while(cont < consoantes.length);
 
        for(String consoante : consoantes){
+           if(consoante!=null)
            JOptionPane.showMessageDialog(null,consoante);
        }
+        JOptionPane.showMessageDialog(null,"Quantiadde de consoantes "+qtdConsoantes);
     }
 }
